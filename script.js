f2c1344acb9c6ba993e15786a56118907d589e82
hanspederse <40687604+hanspederse@users.noreply.github.com>
2020-05-26 20:28:35 +0200

    dette er en test

---
+++ script.js
@@ -0,0 +1,39 @@
+// javascript
+
+
+//Declare a variable called firstVar but don't initialise it with a value.
+var firstvar;
+
+//Declare a variable called name and assign it your first name.
+var firstName = "Hans";
+var lastName = "Pedersen";
+
+
+
+//Declare and initialise a variable with a number value.
+var mycarsage = 5;
+var houseage = 8;
+console.log(mycarsage + houseage);
+
+
+
+//Create a variable called division and initialise it with a value of 20 divided by 5.
+var division = 20;
+
+console.log(division  /5 );
+
+
+//Write code that checks the type of the value "frog".
+console.log(mycarsage < houseage);
+console.log(mycarsage > houseage);
+
+
+var orderHasShipped = 30;
+console.log(orderHasShipped > orderHasShipped);
+
+
+
+for (var counter = 1; counter < 9; counter++) {
+    console.log('Inside the loop:' + counter);
+}
+console.log('Outside the loop:' + counter);
